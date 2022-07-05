d7b76b24092b7b869f5f7d714bffb5187ffb113e
EnteroPositivo <enteropositivo@gmail.com>
2022-07-05 12:40:34 +0200

    rust no-std working sample with vectors and printf

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,40 @@
 #![no_std]
 #![no_main]
+#![feature(default_alloc_error_handler)]
 
-//extern crate no_std_compat as std;
+// no-std print functions
+use libc_print::*;
 
-use core::panic::PanicInfo;
+// allocator
+use libc_alloc::LibcAlloc;
+#[global_allocator]
+static ALLOCATOR: LibcAlloc = LibcAlloc;
+
+// std compatibility
+use no_std_compat::{prelude::v1::*, vec::Vec};
 
 
+#[no_mangle]
+pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
 
+    libc_println!("no-std @Entero{}", "Positivo");
+
+    let mut vec_test: Vec<u8> = Vec::new();
+    vec_test.push(10);
+    vec_test.push(20);
+    vec_test.push(30);
+
+    for v in vec_test{
+        libc_println!("Vector-Item = {}", v);
+    }
+
+    0
+}
 
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let mut x = 6;
-    x= x*2;
-    x=x+1;
-
-   
+fn my_panic(_info: &core::panic::PanicInfo) -> ! {
+    libc_println!("panic!:: {}", _info.to_string().as_str());
     
-
     loop {}
 }
